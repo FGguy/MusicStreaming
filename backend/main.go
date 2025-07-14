afd1dccddce06b3653e44f451868eccf36b6724e
Yan P <y.pilon9@gmail.com>
2025-07-14 15:19:57 -0400

    create default admin user

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	server "music-streaming/server"
+	"os"
 
+	sqlc "music-streaming/sql/sqlc"
+
+	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -18,9 +23,35 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	//TODO: load config file
-	//TODO: Create tables if they dont exist
-	//TODO: Create admin user if he doesnt exist
+	ctx := context.Background()
+
+	adminName, adminNameDefined := os.LookupEnv("ADMIN_NAME")
+	adminPassword, adminPasswordDefined := os.LookupEnv("ADMIN_PASSWORD")
+	if !adminNameDefined || !adminPasswordDefined {
+		log.Fatal("Failed to get admin credentials from ENV. Make sure the variables ADMIN_NAME and ADMIN_PASSWORD are defined in your .env or in the docker-compose file.")
+	}
+
+	conn, err := pgx.Connect(ctx, os.Getenv("POSTGRES_CONNECTION_STRING"))
+	if err != nil {
+		log.Fatalf("Unable to connect to Database in main, Err: %s", err)
+	}
+	defer conn.Close(ctx)
+
+	createTablesScript, err := os.ReadFile("./sql/tables.sql")
+	if err != nil {
+		log.Fatalf("Failed to open script for creating tables, Err: %s", err)
+	}
+
+	_, err = conn.Exec(ctx, string(createTablesScript)) //create all tables
+	if err != nil {
+		log.Fatalf("Failed to create tables in main, Err: %s", err)
+	}
+
+	q := sqlc.New(conn)
+	_, err = q.InsertUser(ctx, sqlc.InsertUserParams{Name: adminName, Password: adminPassword})
+	if err != nil {
+		log.Fatalf("Failed to create admin user, Err: %s", err)
+	}
 
 	server.NewServer().Run(fmt.Sprintf(":%d", PORT))
 }
--- backend/server/authentication.go
+++ backend/server/authentication.go
@@ -1,7 +1,16 @@
 package server
 
+import "github.com/gin-gonic/gin"
+
 /*
 Authentication middlware for:
 	- Authentication
 	- Verifying user got permission for the current endpoint
 */
+
+func (s *Server) WithAuth(c *gin.Context) {
+	//check if user credentials is in cache
+	//if not check if user credential is in db
+	//if in db push to cache
+	//authenticate
+}
